24b4b18ba94c4e0f450616ea2b717e19e5a10a72
Vishz07 <anandnairaok@gmail.com>
2022-12-12 15:55:58 +0530

    Js-Translation-complete

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtinput = document.querySelector("#txt-input");
 var output = document.querySelector("#output");
+var serverurl = "https://api.funtranslations.com/translate/minion.json"
 
+function Translator(text){
+  return serverurl + "?" + "text=" + text
+}
 
+function errorhandler(error){
+  console.log("error occured,error");
 
+}
 
 function clickHandler() {
-  output.innerText = "" + txtinput.value;
-  
+  var inputtxt = txtinput.value;
+
+  fetch(Translator(inputtxt))
+    .then(response => response.json())
+    .then (json => {
+    var translatedjson = json.contents.translated;
+    output.innerText = translatedjson;
+  })
 };
 
 btnTranslate.addEventListener("click",clickHandler);
+
