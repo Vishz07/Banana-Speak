4474323b38b42b39f7acc8dba199165a337c5a81
Vishz07 <anandnairaok@gmail.com>
2022-12-15 17:00:15 +0530

    updated

--- app.js
+++ app.js
@@ -1,27 +1,25 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtinput = document.querySelector("#txt-input");
 var output = document.querySelector("#output");
-var serverurl = "https://api.funtranslations.com/translate/minion.json"
+var serverurl = "https://api.funtranslations.com/translate/minion.json";
 
-function Translator(text){
-  return serverurl + "?" + "text=" + text
+function Translator(text) {
+  return serverurl + "?" + "text=" + text;
 }
 
-function errorhandler(error){
+function errorhandler(error) {
   console.log("error occured,error");
-
 }
 
 function clickHandler() {
   var inputtxt = txtinput.value;
 
   fetch(Translator(inputtxt))
-    .then(response => response.json())
-    .then (json => {
-    var translatedjson = json.contents.translated;
-    output.innerText = translatedjson;
-  })
-};
+    .then((response) => response.json())
+    .then((json) => {
+      var translatedjson = json.contents.translated;
+      output.innerText = translatedjson;
+    });
+}
 
-btnTranslate.addEventListener("click",clickHandler);
-
+btnTranslate.addEventListener("click", clickHandler);
